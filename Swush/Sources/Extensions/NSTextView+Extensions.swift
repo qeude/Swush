592c0d986f42bd107883f748ab5f0f94dd504a73
Quentin Eude <quentin.eude@getaround.com>
2022-01-31 21:12:25 +0100

    🐛 Fixed quotes char in TextEditor
    Fix
    
    fix

---
+++ Swush/Sources/Extensions/NSTextView+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  NSTextView+Extensions.swift
+//  Swush
+//
+//  Created by Quentin Eude on 31/01/2022.
+//
+
+import AppKit
+
+extension NSTextView {
+    open override var frame: CGRect {
+        didSet {
+            self.isAutomaticQuoteSubstitutionEnabled = false
+        }
+    }
+}
